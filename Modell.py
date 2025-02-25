669969a1fcc478a021c15b1bd78e4720b83daa75
annkannn <144692934+annkannn@users.noreply.github.com>
2025-02-25 09:18:17 +0100

    Update Modell.py

--- Modell.py
+++ Modell.py
@@ -1,9 +1,43 @@
+import pygame as pg
+import numpy as np
+
+
 class cell:
-    def __init__(self,posx,posy,lengde,bredde):
+    def __init__(self,posx,posy):
         self.posx = posx
         self.posy = posy
+        
     
 
 class GOL:
-    def __init__(self):
-        pass
+    def __init__(self,lengde = 400 ,bredde = 400):
+        pg.init()
+        self.lengde = lengde
+        self.bredde = bredde
+        self.skjerm = pg.display.set_mode((self.lengde,self.bredde))
+        self.skjerm.fill((255,245,255))
+        self.runnin = True
+        
+    def run(self):
+        
+        while self.runnin:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.runnin = False
+                pg.display.flip()
+        pg.quit()
+        
+game = GOL()
+game.run()
+
+
+
+        
+    
+
+        
+
+
+        
+        
+        
